d4d1d856618f4029cb993b38b7ae030c660eb169
Wataru Inariba <oinari17@gmail.com>
2019-09-25 17:07:20 +0900

    update external libraries

--- include/bgl/util/fmt.hpp
+++ include/bgl/util/fmt.hpp
@@ -2,4 +2,4 @@
 #include "../extlib/fmt/include/fmt/format.h"
 #include "../extlib/fmt/include/fmt/ostream.h"
 #include "../extlib/fmt/include/fmt/ranges.h"
-#include "../extlib/fmt/include/fmt/time.h"
+#include "../extlib/fmt/include/fmt/chrono.h"
